dfbc27bac1d3401ce0d747579e2cc2d3820a385a
ManikpreetSingh <153383684+ManikpreetSingh@users.noreply.github.com>
2024-06-02 18:30:10 +0530

    Update Kadane_Algorithm.cpp

--- Array/Kadane_Algorithm.cpp
+++ Array/Kadane_Algorithm.cpp
@@ -1,1 +1,22 @@
+//QUESTION LINK --> https://leetcode.com/problems/maximum-subarray/description/
 
+// Given an integer array nums, find the  subarray with the largest sum, and return its sum.
+
+//CODE ->
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size(),  maxi = INT_MIN ,  sum =0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(sum > maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+
+    }
+};
