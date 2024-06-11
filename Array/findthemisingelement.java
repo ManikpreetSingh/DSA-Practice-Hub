244b49ffd8efaf2ff25dd9a0e4ce61958543fd90
Jatin Kumar <93573706+JatinGOT@users.noreply.github.com>
2024-06-11 12:16:31 +0530

    Update findthemisingelement.java

--- Array/findthemisingelement.java
+++ Array/findthemisingelement.java
@@ -3,7 +3,7 @@ public class FindtheMissingNumber {
         int n = arr.length;
         int sum =(n*(n+1))/2;
         int sum2 =0;    
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum2+=arr[i];
         }
         int missingSum = sum-sum2;
