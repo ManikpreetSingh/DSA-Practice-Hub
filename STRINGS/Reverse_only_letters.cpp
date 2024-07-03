e77d275e77e9a0ecc976259d83868d72e918d90b
ManikpreetSingh <153383684+ManikpreetSingh@users.noreply.github.com>
2024-07-04 01:41:30 +0530

    Create Reverse_only_letters.cpp

---
+++ STRINGS/Reverse_only_letters.cpp
@@ -0,0 +1,20 @@
+Link -> https://leetcode.com/problems/reverse-only-letters/description/
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l =0 , h = s.length()-1;
+        while(l<=h){
+            if(isalpha(s[l]) && isalpha(s[h])){
+                swap(s[l],s[h]);
+                l++;
+                h--;
+            }else if(!isalpha(s[l])){
+                l++;
+            }else{
+                h--;
+            }
+        }
+        return s;
+    }
+};
