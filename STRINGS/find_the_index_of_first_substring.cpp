53fee3a7f8c8ce79397cc022c6a243ae129e5c3a
ManikpreetSingh <153383684+ManikpreetSingh@users.noreply.github.com>
2024-07-08 00:23:56 +0530

    Create find_the_index_of_first_substring.cpp

---
+++ STRINGS/find_the_index_of_first_substring.cpp
@@ -0,0 +1,19 @@
+//Link - > https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n  = haystack.size();
+        int m = needle.size();
+        for(int i = 0 ; i <= n-m; i++){
+            for(int j = 0 ; j<m;j++){
+                if(needle[j] != haystack[i+j]){
+                    break;
+                }
+                if(j == m - 1 ){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
